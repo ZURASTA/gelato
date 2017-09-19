76e997c98ec4fbcad30c8b6ccd19db1d40955688
Evan <devillordking@gmail.com>
2017-09-19 13:51:51 +1000

    Add skeleton vc

---
+++ gelato/TrackingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TrackingViewController.swift
+//  gelato
+//
+//  Created by EvanTsai on 2017/9/19.
+//  Copyright © 2017年 Zurasta. All rights reserved.
+//
+
+import UIKit
+
+class TrackingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
