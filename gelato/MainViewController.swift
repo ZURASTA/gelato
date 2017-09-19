0eac56e27166fd920b533005d9265f79fba8d075
Evan <devillordking@gmail.com>
2017-09-19 13:41:55 +1000

    add filter vc

--- gelato/MainViewController.swift
+++ gelato/MainViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     @IBOutlet weak var titleItem: UINavigationItem!
     @IBOutlet weak var contentView: UIView!
