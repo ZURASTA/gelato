e7b92bcb380ba2c51680fe60b0b893470e901b96
Evan <devillordking@gmail.com>
2017-11-05 19:23:06 +1100

    Add menu bars to main view

--- gelato/MainViewController.swift
+++ gelato/MainViewController.swift
@@ -15,6 +15,10 @@ class MainViewController: UIViewController, SushiProtocol{
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var contentHeightConstraint: NSLayoutConstraint!
     
+    @IBOutlet weak var menuViewWidthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var menuView: UIView!
+    
     var rows = 3
     let rowHeight = 322
     var sushi = [SushiRoll]()
@@ -35,6 +39,8 @@ class MainViewController: UIViewController, SushiProtocol{
         /* do not show title on back button on navbar in next VC */
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.plain, target:nil, action:nil)
         
+        /* set up menu items*/
+        setupMenuButtons()
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -59,6 +65,42 @@ class MainViewController: UIViewController, SushiProtocol{
         contentView.frame = CGRect(x: contentView.frame.origin.x, y: contentView.frame.origin.y, width: contentView.frame.width, height: height)
     }
 
+    // MARK: - Menu Items
+    private func setupMenuButtons(){
+
+        /* initailise menu view content */
+        let button0 = UIButton(frame: CGRect(x: 10, y: 0, width: 70, height: 50))
+        button0.setTitle("Entree", for: .normal)
+        button0.setTitleColor(UIColor.black, for: .normal)
+        let button1 = UIButton(frame: CGRect(x: 90, y: 0, width: 100, height: 50))
+        button1.setTitle("Main Dishes", for: .normal)
+        button1.setTitleColor(UIColor.black, for: .normal)
+        let button2 = UIButton(frame: CGRect(x: 200, y: 0, width: 50, height: 50))
+        button2.setTitle("Sides", for: .normal)
+        button2.setTitleColor(UIColor.black, for: .normal)
+        let button3 = UIButton(frame: CGRect(x: 260, y: 0, width: 50, height: 50))
+        button3.setTitle("Kids", for: .normal)
+        button3.setTitleColor(UIColor.black, for: .normal)
+        let button4 = UIButton(frame: CGRect(x: 320, y: 0, width: 70, height: 50))
+        button4.setTitle("Dessers", for: .normal)
+        button4.setTitleColor(UIColor.black, for: .normal)
+        let button5 = UIButton(frame: CGRect(x: 400, y: 0, width: 70, height: 50))
+        button5.setTitle("Drinks", for: .normal)
+        button5.setTitleColor(UIColor.black, for: .normal)
+        
+        menuView.addSubview(button0)
+        menuView.addSubview(button1)
+        menuView.addSubview(button2)
+        menuView.addSubview(button3)
+        menuView.addSubview(button4)
+        menuView.addSubview(button5)
+        
+        /* update the frame of menu view*/
+        menuView.frame = CGRect(x: 0, y: 0, width: 480 , height: menuView.frame.height)
+        menuViewWidthConstraint.constant = 480
+    }
+    
+    
     // MARK: - Delegate methods
     
     func didTapBiscuit(sender: Any, bID: Int, rID: Int) {
